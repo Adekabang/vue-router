c40fc79f13bd3a54ec1d90bfa952b322264fd9ab
Mohammad Raska <iban.ninja@gmail.com>
2020-05-31 05:19:47 +0700

    add invoices and redirect login

--- vue-travel-app/src/router/index.js
+++ vue-travel-app/src/router/index.js
@@ -49,6 +49,12 @@ const routes = [
     meta: {requiresAuth: true}
   },
   {
+    path: "/invoices",
+    name: "invoices",
+    component: () => import( /* webpackChunkName: "Invoices" */ "../views/Invoices"),
+    meta: {requiresAuth: true}
+  },
+  {
     path: "404",
     alias: "*",
     name: "notFound",
@@ -85,7 +91,8 @@ router.beforeEach((to, from, next) => {
   if(to.matched.some(record => record.meta.requiresAuth)){
     if(!store.user){
       next({
-        name: "login"
+        name: "login",
+        query: {redirect: to.fullPath}
       })
     }
     else{
